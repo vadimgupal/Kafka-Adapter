811e7688e714bd7a72fafd4678043e9348e7587e
vadimgupal <114927468+vadimgupal@users.noreply.github.com>
2026-03-28 00:24:03 +0300

    feat: add web server

---
+++ httpServer/src/main/java/practice/HttpServerApplication.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HttpServerApplication.class, args);
+    }
+}
